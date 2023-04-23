cd86db8f92e2feb52a5a3c249986bffa37efb168
cMardc <130239955+cMardc@users.noreply.github.com>
2023-04-23 18:23:07 +0400

    Add files via upload

---
+++ Fast File Reader.cpp
@@ -0,0 +1,43 @@
+﻿#include "Fast File Reader.h"
+using namespace std;
+namespace fs = filesystem;
+
+
+int main() //Run Only On Execute
+{
+	string target; //target Drive Path
+	string path; //Text File Path
+	string name; //Name OF Fıle
+	string text; //Text Insıde File
+	cout << "\nEnter Drive Path : ";
+	getline(cin, target); 
+	cout << "\nEnter TextFile Path : ";
+	getline(cin, path); 
+	ofstream textFile(path);
+	bool open = textFile.is_open();
+	if (!open)
+	{
+		cout << "\nWrong Path Entered!";
+		return 0; //End Program Here
+	}
+	for (const auto& targetFS : fs::directory_iterator(target)) 
+	{
+		//get Path that Includes Target Path
+		name = getName(targetFS.path().generic_string());
+		//Get Name Of File
+		ifstream targetFile(targetFS.path().generic_string());
+		//Check If File Really Exsists
+		if (!targetFile.is_open())
+		{
+			cout << "One File Failed During Upload.";
+		}
+		else
+		{
+
+			getline(targetFile, text);//Read Text Inside File
+			textFile << name << "\n" << text << "\n"; //Include It On File
+		}
+	}
+	textFile.close(); //Close File
+	return 0; //End Program
+}
---
+++ Fast File Reader.h
@@ -0,0 +1,41 @@
+﻿#pragma once
+
+//Libraries Used
+#include <iostream> //Input/Output Stream For Console
+#include <string>   //For String
+#include <fstream>  //File Stream
+#include <filesystem> //For Controling Files
+
+
+using namespace std;//Standart Namespace
+
+//Get File's Name On Given Path
+string getName(const string target)
+{
+	size_t size = target.size(); //Size Of Given Path
+	size_t lastPos = size; //Position Of Backslash(\)
+	char symbol; //Char For Symbol On Given Index
+	string name; //Result
+	for (size_t i = 0; i < size; i++) //Loop To Get Chars
+	{
+		symbol = target[i]; //Get Char On Given Index Of Path
+		if (symbol == '\\') 
+		{
+			lastPos = i; //Get Position Of Backslash(\)
+		}
+	}
+	for (size_t i = lastPos; i < size; i++) //Loop To Get Name
+	{
+		symbol = target[i]; //Get Char On Given Index Of Path
+		if (symbol == '.') //IF Symbol Is Dot
+		{
+			break; //Vreak Because Format Comes After Dot
+		}
+		else
+		{
+			name += symbol; //Add It To Name
+		}
+	}
+	return name; //Give Name
+}
+
